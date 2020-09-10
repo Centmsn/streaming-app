8828fb88c7a0a9ec1ddb88ffe92827cfb3d49088
Wojtek Rygorowicz <wojciech.rygorowicz@gmail.com>
2020-09-10 12:01:27 +0200

    add redux-form to project

--- src/componets/streams/StreamCreate.js
+++ src/componets/streams/StreamCreate.js
@@ -1,10 +1,68 @@
 import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends Component {
   state = {};
+
+  renderError = ({ touched, error }) => {
+    console.log(touched, error);
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  };
+
+  renderInput = ({ label, input, meta }) => {
+    console.log(meta);
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  handleSubmit = (values) => {
+    console.log(values);
+  };
+
   render() {
-    return <div>StreamCreate</div>;
+    return (
+      <form
+        className="ui form error"
+        onSubmit={this.props.handleSubmit(this.handleSubmit)}
+      >
+        <Field name="title" component={this.renderInput} label="Enter title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter descrption"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
   }
 }
 
-export default StreamCreate;
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Cannot be empty";
+  }
+
+  if (!values.description) {
+    errors.description = "Cannot be empty";
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  form: "streamCreate",
+  validate: validate,
+})(StreamCreate);
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers";
 import App from "./componets/App";
 
 import "semantic-ui-css/semantic.min.css";
 
-const store = createStore(reducers);
+const composeEnch = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnch(applyMiddleware()));
 
 ReactDOM.render(
   <Provider store={store}>
